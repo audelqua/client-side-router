204b386ed5c49b988f86a61e7b9c102b1f07a603
audrey <mbozorgi604@gmail.com>
2023-02-12 01:11:08 +0330

    add event listener on some element gotten by id in index html

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ import routes from './routes.js'
 const router = new Router(routes);
 window.router = router
 
+document.getElementById('some-element-using-event-listener').addEventListener('click', customCallbackFunction)
 
+function customCallbackFunction() {
+    console.log('hi there!');
+}
